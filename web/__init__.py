cc3e7965e70e68e320e9dcee50bf3c74ffc4abb0
Andrey Vladykov <andrey.vladykov@kaspersky.com>
2022-03-28 21:21:13 +0300

    Add sign up flow.

--- web/__init__.py
+++ web/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from os import path
 import os
 
 load_dotenv()
 
+db = SQLAlchemy()
+DB_NAME = 'database.sqlite'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ['FLASK_APP_SECRET_KEY']
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -15,4 +22,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists('web/' + DB_NAME):
+        db.create_all(app=app)
+        print('Database created!')
--- web/auth.py
+++ web/auth.py
@@ -1,10 +1,12 @@
-from socket import errorTab
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -14,6 +16,24 @@ def logout():
     return 'logout'
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'GET':
+        return render_template('signup.html')
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if '@' not in email:
+            flash('Incorrect email.', category='error')
+        if password1 != password2:
+            flash('Passwords does not match.', category='error')
+        else:
+            new_user = User(email=email, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('views.home'))
+
+        return render_template('signup.html')
